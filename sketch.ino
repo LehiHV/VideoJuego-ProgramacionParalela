29067034ece9be82384ab03fce3b69172f8eebf2
Lehi Alejandro Hernández Velasco <113217024+LehiHV@users.noreply.github.com>
2024-10-10 12:24:53 -0600

    Update sketch.ino
    Cambios en el funcionamiento del manejo del panel.h

--- sketch.ino
+++ sketch.ino
@@ -1,30 +1,12 @@
-#include <Jostick.h>
+#include <Panel.h>
 
-Joystick joystick;
+Panel panel;
 
 void setup() {
   Serial.begin(115200);
-  
-  // Configurar pines
-  pinMode(VERT_PIN, INPUT);
-  pinMode(HORZ_PIN, INPUT);
-  pinMode(SEL_PIN, INPUT_PULLUP); 
-  
+  panel.init();
 }
 
 void loop() {
-  // Leer valores del joystick
-  int x = joystick.getX();
-  int y = joystick.getY();
-  bool pressed = joystick.isPressed();
-
-  // Mostrar valores por el puerto serie
-  Serial.print("X: ");
-  Serial.print(x);
-  Serial.print(" | Y: ");
-  Serial.print(y);
-  Serial.print(" | Pressed: ");
-  Serial.println(pressed);
-
-  delay(1000); // Retraso para evitar que los valores se actualicen demasiado rápido
+  panel.update();
 }
